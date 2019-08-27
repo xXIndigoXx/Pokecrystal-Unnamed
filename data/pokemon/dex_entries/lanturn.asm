d93ac95d1e68f62a3d69fc6d170a021d84c8593d
xXIndigoXx <54562824+xXIndigoXx@users.noreply.github.com>
2019-08-27 00:21:17 -0700

    Add files via upload

--- data/pokemon/dex_entries/lanturn.asm
+++ data/pokemon/dex_entries/lanturn.asm
@@ -1,10 +1,10 @@
 	db "LIGHT@" ; species name
 	dw 311, 500 ; height, weight
 
-	db   "This #MON uses"
+	db   "LANTURN uses"
 	next "the bright part of"
 	next "its body, which"
 
 	page "changed from a"
 	next "dorsal fin, to"
-	next "lure prey.@"
+	next "Guide Others.@"
