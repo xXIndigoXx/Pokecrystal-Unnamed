f8dc21644660ccdb383d9851a9c87eb6957d6fbb
xXIndigoXx <54562824+xXIndigoXx@users.noreply.github.com>
2019-08-29 12:34:13 -0700

    Update and rename misdreavus.asm to RinRin.asm

---
+++ data/pokemon/dex_entries/RinRin.asm
@@ -0,0 +1,10 @@
+	db "Bell_Cat@" ; species name
+	dw 104, 15 ; height, weight
+
+	db   "People Love RinRin"
+	next "It rings the bell"
+	next "To show affection."
+
+	page "Its Numbers are l-"
+	next "ow due to the bell"
+	next "Alerting prey.@"
